5e1bce1585d29c4782e19694c876b2b57e8f4007
Patrick Gundlach <gundlach@speedata.de>
2021-09-11 15:21:34 +0200

    Encode key=value also with bool, float64
    A few more changes:
     * Use io.Reader and io.Writer as an argument in ToXML
     * Add license to file header

--- json2xml.go
+++ json2xml.go
@@ -1,14 +1,30 @@
 package json2xml
 
+// Copyright 2021 Patrick Gundlach
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+// AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+// OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"io"
-	"strings"
 )
 
-// encodeString encodes an element named string with the attribute key="<value of key>" if key is not empty.
+// encodeString encodes str as an element named entry with the attribute key="<value of key>" if key is not empty.
 func encodeString(enc *xml.Encoder, str string, key string) error {
 	stringElt := xml.StartElement{Name: xml.Name{Local: "entry"}}
 	var err error
@@ -24,22 +40,23 @@ func encodeString(enc *xml.Encoder, str string, key string) error {
 	return enc.EncodeToken(stringElt.End())
 }
 
-// ToXML returns a string and an error, perhaps nil, from the json encoded in the argument.
-func ToXML(r io.Reader) (string, error) {
-	var w strings.Builder
+// ToXML reads the JSON file from jsonInput and writes to xmlOutput.
+func ToXML(jsonInput io.Reader, xmlOutput io.Writer) error {
 	var err error
-	dec := json.NewDecoder(r)
+	dec := json.NewDecoder(jsonInput)
 
-	enc := xml.NewEncoder(&w)
+	enc := xml.NewEncoder(xmlOutput)
 	root := xml.StartElement{}
 	root.Name = xml.Name{Local: "data"}
 	mapElt := xml.StartElement{Name: xml.Name{Local: "map"}}
 	aryElt := xml.StartElement{Name: xml.Name{Local: "array"}}
+	// inMap is a stack with the last element shows if we are currently
+	// in a map. This is to insert key=".." attributes.
 	inMap := []bool{false}
 
 	err = enc.EncodeToken(root)
 	if err != nil {
-		return "", err
+		return err
 	}
 
 	var key string
@@ -47,7 +64,7 @@ encodeLoop:
 	for {
 		tok, err := dec.Token()
 		if err != nil && err != io.EOF {
-			return "", err
+			return err
 		}
 		if tok == nil {
 			break encodeLoop
@@ -82,7 +99,7 @@ encodeLoop:
 				if key != "" {
 					err = encodeString(enc, key, "")
 					if err != nil {
-						return "", err
+						return err
 					}
 					key = ""
 				}
@@ -92,7 +109,7 @@ encodeLoop:
 				if key != "" {
 					err = encodeString(enc, key, "")
 					if err != nil {
-						return "", err
+						return err
 					}
 					key = ""
 				}
@@ -101,7 +118,6 @@ encodeLoop:
 		case string:
 			if inMap[len(inMap)-1] {
 				if key != "" {
-					// string = string
 					encodeString(enc, t, key)
 					key = ""
 				} else {
@@ -111,17 +127,22 @@ encodeLoop:
 			} else {
 				err = encodeString(enc, t, "")
 				if err != nil {
-					return "", err
+					return err
 				}
 			}
 		case float64, bool:
-			encodeString(enc, fmt.Sprint(t), "")
+			if key != "" {
+				encodeString(enc, fmt.Sprint(t), key)
+				key = ""
+			} else {
+				encodeString(enc, fmt.Sprint(t), "")
+			}
 		default:
-			fmt.Println(t)
+			panic("not implemented")
 		}
 	}
 
 	enc.EncodeToken(root.End())
 	enc.Flush()
-	return w.String(), nil
+	return nil
 }
--- json2xml_test.go
+++ json2xml_test.go
@@ -3,6 +3,7 @@ package json2xml
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -11,13 +12,14 @@ func TestSimple(t *testing.T) {
 	if err != nil {
 		t.Error("Could not read test data simple.json")
 	}
-	str, err := ToXML(f)
+	var sb strings.Builder
+	err = ToXML(f, &sb)
 	if err != nil {
 		t.Error(err)
 	}
 
 	expected := `<data><map><array key="whatever"><entry>foo</entry><entry>3.45</entry><entry>bar</entry><entry>1</entry></array><map key="something"><entry key="another">object</entry><array key="and an"><entry>array</entry></array></map></map></data>`
-	if str != expected {
+	if str := sb.String(); str != expected {
 		t.Errorf("ToXML() got: %q, want %q", str, expected)
 	}
 }
